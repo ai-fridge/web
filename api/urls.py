93d8e566d37a9a252642c158318c5b3f8fd610f2
Ren syuan Huang <ren@infiniumone.com>
2019-02-17 21:58:32 +0800

    Modify yolo-api to api/object/detection/

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,6 @@ app_name = 'api'
 urlpatterns = [
     path('register/',views.register,name='register'),
     path('user_login/',views.user_login,name='user_login'),
-    path('face/recognition/', views.verify_face_recognition, name='verify_face_recognition')
+    path('face/recognition/', views.verify_face_recognition, name='verify_face_recognition'),
+    path('object/detection/',views.Object_Detection,name='object_detection')
 ]
