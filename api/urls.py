8b08bf3d050d0eebeca7eb37cd00df95fb731ae6
Eder Fan <fanedercode@gmail.com>
2019-02-17 16:57:23 +0800

    integrate face recognition

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,6 @@ app_name = 'api'
 
 urlpatterns = [
     path('register/',views.register,name='register'),
-    path('user_login/',views.user_login,name='user_login')
+    path('user_login/',views.user_login,name='user_login'),
+    path('face/recognition/', views.verify_face_recognition, name='verify_face_recognition')
 ]
